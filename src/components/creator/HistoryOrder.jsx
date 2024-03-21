07eadbd192f522dd1c8242173f59768c2a9dfc0a
votrongvuong672 <votrongvuong001@gmail.com>
2024-03-21 11:44:05 +0700

    thieu field history

--- src/components/creator/HistoryOrder.jsx
+++ src/components/creator/HistoryOrder.jsx
@@ -42,6 +42,25 @@ const HistoryOrder = () => {
     }
     return result;
   };
+  const formatDate = (dateTimeString) => {
+    const date = new Date(dateTimeString);
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    // Đảm bảo các số có hai chữ số bằng cách thêm số 0 nếu cần
+    const formattedDay = day < 10 ? '0' + day : day;
+    const formattedMonth = month < 10 ? '0' + month : month;
+    const formattedHours = hours < 10 ? '0' + hours : hours;
+    const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+
+    // Định dạng lại chuỗi ngày/tháng/năm giờ:phút
+    const formattedDateTime = `${formattedDay}/${formattedMonth}/${year} ${formattedHours}:${formattedMinutes}`;
+
+    return formattedDateTime;
+  };
 
 
 
@@ -65,6 +84,7 @@ const HistoryOrder = () => {
                       <th>Giá (VND)</th>
                       <th>Người mua</th>
                       <th>Email</th>
+                      <th>Thời gian đặt hàng</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -77,6 +97,7 @@ const HistoryOrder = () => {
                           <td>{formatPrice(artwork.orderDetailPrice)}</td>
                           <td>{artwork.artwork.user.accountName}</td>
                           <td>{artwork.artwork.user.email}</td>
+                          <td>{formatDate(artwork.order.orderDate)}</td>
                           {/* <td>
                             <button onClick={() => handleRemove(artwork.artworkId)}>Delete</button>
                           </td>
